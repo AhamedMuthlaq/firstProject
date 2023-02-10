aa8b9297ea7c877e5ba13c648da56ea8a8fbb765
ahamedmuthlaq <ahamedmuthlaq001@gmail.com>
2023-02-10 18:12:35 +0530

    switched to prod

--- Add.java
+++ Add.java
@@ -2,11 +2,11 @@ class Add {
     public static void main(String[] args) {
         int a = 15;
         int b = 6;
-        System.out.println(add(a, b));
+        System.out.println(prod(a, b));
 
     }
 
-    private static int add(int a, int b) {
-        return a + b;
+    private static int prod(int a, int b) {
+        return a * b;
     }
 }
